712dc100baffb7eb0a78b97ef9e4fee27bae7181
Katherine Ebel <kathyebel@me.com>
2021-11-18 21:03:43 -0500

    set names for themes in theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider themes={['1', '2', '3']} enableSystem>
       <Layout>
         <Component {...pageProps} />
       </Layout>
